52bca62861bd62425a4283d12e891a6c1695dbd4
Jaishnav K <jaishnavkalmangi@gmail.com>
2025-12-01 12:45:07 +0530

    initialization

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> dq;
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_back(3);
+    dq.push_back(4);
+    cout << dq.front()<<endl;
+    cout << dq.back();
+    return 0;
+}
