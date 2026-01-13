cfbd45f26691bfc061e39ae9a2db8a6f6f46dd95
Jaishnav K <jaishnavkalmangi@gmail.com>
2026-01-13 14:28:10 +0530

    shifting of folder

--- sample.cpp
+++ sample.cpp
@@ -1,13 +1,37 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <limits.h>
 using namespace std;
 
-int main()
-{
-    int arr[] = {3,6,2,9,5,1,7};
-    sort(arr, arr+7);
-    for(int i: arr){
-        cout << i << " ";
+template <typename T>
+void printVector(const vector<T>& v) {
+    cout << "[";
+    for (int i = 0; i < (int)v.size(); i++) {
+        cout << v[i];
+        if (i + 1 < v.size()) cout << ",";
     }
+    cout << "]";
+}
+
+template <typename T>
+void print2DVector(const vector<vector<T>>& grid) {
+    cout << "[";
+    for (int i = 0; i < grid.size(); i++) {
+        printVector(grid[i]);
+        if (i + 1 < grid.size()) cout << ",";
+    }
+    cout << "]";
+}
+
+void printString(const string &s) {
+    cout << "\"" << s << "\"";
+}
+
+
+// Paste Solution class here
+
+
+int main() {
+    cout << "A";
     return 0;
 }
