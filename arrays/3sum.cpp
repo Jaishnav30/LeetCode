553e79c6315fecf507ad7656eccb2186969c7f66
Jaishnav K <jaishnavkalmangi@gmail.com>
2026-01-14 10:57:04 +0530

    leetcode_15

--- arrays/3sum.cpp
+++ arrays/3sum.cpp
@@ -32,22 +32,47 @@ void printString(const string &s) {
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> res;
+        // set<vector<int>> res;
+        // int n=nums.size();
+        // for(int i=0; i<n-2; i++){
+        //     set<int> tmp;
+        //     for(int j=i+2; j<n; j++){
+        //         tmp.insert(nums[j-1]);
+        //         int othr=-(nums[i]+nums[j]);
+        //         if(tmp.find(othr)!=tmp.end()){
+        //             vector<int>v = {nums[i],nums[j],othr};
+        //             sort(v.begin(), v.end());
+        //             res.insert(v);
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> fres(res.begin(), res.end());
+        // return fres;
+
         int n=nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
         for(int i=0; i<n-2; i++){
-            set<int> tmp;
-            for(int j=i+2; j<n; j++){
-                tmp.insert(nums[j-1]);
-                int othr=-(nums[i]+nums[j]);
-                if(tmp.find(othr)!=tmp.end()){
-                    vector<int>v = {nums[i],nums[j],othr};
-                    sort(v.begin(), v.end());
-                    res.insert(v);
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            int prv_j=INT32_MIN;
+            while(j<k){
+                if(nums[j]==prv_j){
+                    j++;
+                    continue;
+                } 
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    prv_j=nums[j];
+                    j++;
                 }
+                else if(sum<0) j++;
+                else k--;
             }
         }
-        vector<vector<int>> fres(res.begin(), res.end());
-        return fres;
+        return res;
     }
 };
 
@@ -56,7 +81,7 @@ int main() {
 
     // custom input
     // ex: vector<int> nums = {}; or vector<vector<int>> matrix = {}; 
-    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<int> nums = {-6, -4, -2, 0, 0, 2, 4, 6};
     // function call
     // ex: vector<int> res or int res = sol.moveZeroes(nums);
     vector<vector<int>> res = sol.threeSum(nums);
